b8cca6ce254d5c6134307729441c9a583d3056d8
Vinay <navik.vinay12@gmail.com>
2022-11-15 19:14:40 +0530

    Fibonacci program

---
+++ FibonacciSeries1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms6thAssignment
+{
+    internal class FibonacciSeries1
+    {
+        public static void FibonacciSeries()
+        {
+            Console.WriteLine("How many elements of fabonacci numbers you wish to see");
+            int lastNum = Convert.ToInt32(Console.ReadLine());
+            int previous = 0, next = 1;
+            Console.Write("Fabonacci Series : " +previous + " " + next);
+            for (int i = 3; i <= lastNum; i++)
+            {
+                int sum = previous + next;
+                Console.Write(" " + sum);
+                previous = next;
+                next = sum;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LogicalPrograms6thAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 : Fibonacci Series\n");
+            int switchOptions=Convert.ToInt32(Console.ReadLine());
+            switch (switchOptions)
+            {
+                case 1:
+                    FibonacciSeries1.FibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option entered!");
+                    break;
+            }
+        }
+    }
+}
