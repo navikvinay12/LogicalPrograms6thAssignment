b83debc09dc2e77809107fac224074b8120966e7
Vinay <navik.vinay12@gmail.com>
2022-11-15 19:22:37 +0530

    finding perfect num -program

---
+++ PerfectNumber2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms6thAssignment
+{
+    internal class PerfectNumber2
+    {
+        public static void PerfectNumber()
+        {
+            Console.WriteLine("Enter any number to check and confirm if its a perfect number or not");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int temp = 0;
+            for (int i=1; i<num; i++)
+            {
+                if (num%i==0)
+                {
+                    temp=temp + i;
+                }
+            }
+            if (temp==num)
+            {
+                Console.WriteLine($"{num} is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a perfect number");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press 1 : Fibonacci Series\n");
+            Console.WriteLine("Welcome ! Please use the folowing options as per your choice\n" +
+                "Press 1 : Fibonacci Series\n" +
+                "Press 2 : Perfect Number\n");
             int switchOptions=Convert.ToInt32(Console.ReadLine());
             switch (switchOptions)
             {
                 case 1:
                     FibonacciSeries1.FibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNumber2.PerfectNumber();
                     break;
                 default:
                     Console.WriteLine("Invalid Option entered!");
