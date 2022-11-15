e7450f550e617999dd48894b44b810e7b5440ea6
Vinay <navik.vinay12@gmail.com>
2022-11-15 19:32:23 +0530

    program to reverse no

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
             Console.WriteLine("Welcome ! Please use the folowing options as per your choice\n" +
                 "Press 1 : Fibonacci Series\n" +
                 "Press 2 : Perfect Number\n" +
-                "Press 3 : Prime Number\n");
+                "Press 3 : Prime Number\n" +
+                "Press 4 : Reverse a Number\n");
             int switchOptions=Convert.ToInt32(Console.ReadLine());
             switch (switchOptions)
             {
@@ -19,6 +20,9 @@
                     break;
                 case 3:
                     PrimeNumber3.PrimeNumber();
+                    break;
+                case 4:
+                    ReverseNumber4.ReverseNumber();
                     break;
                 default:
                     Console.WriteLine("Invalid Option entered!");
---
+++ ReverseNumber4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms6thAssignment
+{
+    internal class ReverseNumber4
+    {
+        public static void ReverseNumber()
+        {
+            Console.WriteLine("Enter number to get it reversed.");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int revNum = 0;
+            Console.WriteLine($"Original number is {num}");
+            while (num != 0)
+            {
+                int rem = num % 10;
+                revNum = (revNum * 10) + rem;
+                num = num / 10;
+            }
+            Console.WriteLine($"Reverse number is {revNum}");
+        }
+    }
+}
